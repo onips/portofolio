5e67d1d430c877a1586ca4795d89d0817a1b7cbe
onips <oni.user01@gmail.com>
2024-11-12 08:31:39 +0700

    init

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Ini PC saya, sejak SMK kebanyakan bekerja & main game di PC ini, beberapa project saya buat dengan PC ini, untuk list projectnya ada di ruangan sebelah... `,
+
+  "cs-degree": `Ini adalah Izajah saya. Saya lulusan Politeknik Negeri Cilacap, prodi D3 Teknik Informatika. Saya menggantungnya di dinding karena saya bangga karenanya. Meski sampai saat ini saya tidak jago ngoding (cuma modal copas) tetapi saya pikir kuliah di PNC memberi saya landasan yang baik`,
+  "sofa-table": `Itu sofaku. Saya suka bersantai di sini dan menonton YouTube / TV. Di Youtube saya suka nonton channel DEANKT & WPU. Di TV saya cuma nonton bola, btw saya fans MU (3x Juara UCL)`,
+
+  tv: `Hai, Selamat Datang di Rumah saya, Perkenalkan saya Oni (23), saya tinggal di Ciamis dan sekarang bekerja di Cilacap sebagai editor buku. Hobi saya diantaranya : memancing, fotograpi dan membuat program/apliaksi sederhana`,
+  bed: `Di sinilah saya tidur. Mimpi dan ide-ide hebat datang ketika saya sedang berbaring di tempat tidur. Diantara adalah saya bermimpi ingin membuat game-game pixel seperti Stardey Valley`,
+  resume: `404`,
+  projects: `Beberapa project yang pernah saya kerjakan, diantaranya : <br> 1. Aplikasi Kasir Kafe Berbasis Desktop dengan Bahasa C#<br> 2. Sistem Informasi Pembelian Tiket Kereta Berbasis Website <br> 3. Sistem Informasi Tabungan Siswa Berbasis Website <br> 4. Sistem Informasi Pembayaran SPP Berbasis Website <br> 5. Sistem Informasi Praktik Kerja Lapangan Berbasis Website <br> 6. Sistem Informasi Edit Buku Berbasis Website`,
+  library: `Ada beberapa Skill yang saya kuasai diantaranya : Membuat aplikasi berbasis web & desktop, Membuat design/mockup web di Figma & Balsamiq, menggunakan Microsoft Office (Word, Excel, PowerPoint), Melakukan editing di aplikasi Lightroom & Canva?`,
+  exit: `Jika Anda ingin keluar dari portofolio Oni, tutup saja tabnya.`,
+};
