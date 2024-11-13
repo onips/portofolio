379874bc3ffb45ab2b4c69656ec6a71fd60aeaba
onips <oni.user01@gmail.com>
2024-11-13 08:59:13 +0700

    update dialog

--- src/constants.js
+++ src/constants.js
@@ -9,19 +9,18 @@ export const dialogueData = {
   tv: `Ini TV di rumah ku. Aku suka bersantai di sofa sambil menonton YouTube / TV. Di Youtube aku suka nonton channel DEANKT & WPU. Di TV aku cuma nonton bola, btw aku fans MU (3x Juara UCL)`,
   bed: `Di sinilah aku tidur. Mimpi dan ide-ide hebat datang ketika aku sedang berbaring di tempat tidur. Diantara adalah aku bermimpi ingin membuat game-game pixel seperti Stardey Valley`,
   resume: `404`,
-  projects: `Beberapa project yang pernah aku kerjakan, diantaranya : <br> 1. Aplikasi Kasir Kafe Berbasis Desktop dengan Bahasa C#<br> 2. Sistem Informasi Pembelian Tiket Kereta Berbasis Website <br> 3. Sistem Informasi Tabungan Siswa Berbasis Website <br> 4. Sistem Informasi Pembayaran SPP Berbasis Website <br> 5. Sistem Informasi Praktik Kerja Lapangan Berbasis Website <br> 6. Sistem Informasi Edit Buku Berbasis Website`,
+  projects: `Beberapa project yang pernah aku kerjakan, diantaranya : 
+              <br> 1. Sistem Informasi Edit Buku Berbasis Website - PHP (2024)
+              <br> 2. Sistem Informasi Pembayaran SPP Berbasis Website - PHP (2023)
+              <br> 3. Sistem Informasi Praktik Kerja Lapangan Berbasis Website - PHP (2023) 
+              <br> 4. Sistem Informasi Tabungan Siswa Berbasis Website (2023)
+              <br> 5. Aplikasi Kasir Kafe Berbasis Desktop - C# (2022)
+              <br> 6. Sistem Informasi Klinik Berbasis Website - Python Flask (2021)
+              <br> 7. Sistem Informasi Data Covid dengan API Berbasis Website - Python Flask  (2021)
+              <br> 8. Sistem Informasi Pembelian Tiket Kereta Berbasis Website -PHP (2019)
+              `,
   library: `Ada beberapa Skill yang aku kuasai diantaranya : Membuat aplikasi berbasis web & desktop, Membuat design/mockup web di Figma & Balsamiq, menggunakan Microsoft Office (Word, Excel, PowerPoint), Melakukan editing di aplikasi Lightroom & Canva?`,
   exit: `Jika Kamu ingin keluar dari web portofolio ini, tutup saja tabnya.`,
 };
 
 
-`Beberapa project yang pernah aku kerjakan, diantaranya : 
-<br> 1. Sistem Informasi Edit Buku Berbasis Website - PHP (2024)
-<br> 2. Sistem Informasi Pembayaran SPP Berbasis Website - PHP (2023)
-<br> 3. Sistem Informasi Praktik Kerja Lapangan Berbasis Website - PHP (2023) 
-<br> 4. Sistem Informasi Tabungan Siswa Berbasis Website (2023)
-<br> 5. Aplikasi Kasir Kafe Berbasis Desktop - C# (2022)
-<br> 6. Sistem Informasi Klinik Berbasis Website - Python Flask (2021)
-<br> 7. Sistem Informasi Data Covid dengan API Berbasis Website - Python Flask  (2021)
-<br> 8. Sistem Informasi Pembelian Tiket Kereta Berbasis Website -PHP (2019)
-`
