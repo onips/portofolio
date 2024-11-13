88415ce0145946e9684fecad4c905805ce5ef1e4
onips <oni.user01@gmail.com>
2024-11-13 08:38:57 +0700

    update dialog

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,17 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `Ini PC saya, sejak SMK kebanyakan bekerja & main game di PC ini, beberapa project saya buat dengan PC ini, untuk list projectnya ada di ruangan sebelah... `,
+  pc: `Ini PCku, sejak SMK kebanyakan bekerja & main game di PC ini, beberapa project aku buat dengan PC ini, untuk list projectnya ada di ruangan sebelah... `,
 
-  "cs-degree": `Ini adalah Izajah saya. Saya lulusan Politeknik Negeri Cilacap, prodi D3 Teknik Informatika. Saya menggantungnya di dinding karena saya bangga karenanya. Meski sampai saat ini saya tidak jago ngoding (cuma modal copas) tetapi saya pikir kuliah di PNC memberi saya landasan yang baik`,
-  "sofa-table": `Itu sofaku. Saya suka bersantai di sini dan menonton YouTube / TV. Di Youtube saya suka nonton channel DEANKT & WPU. Di TV saya cuma nonton bola, btw saya fans MU (3x Juara UCL)`,
+  "cs-degree": `Ini adalah Izajahku. Lulusan Politeknik Negeri Cilacap, prodi D3 Teknik Informatika. Aku menggantungnya di dinding karena bangga karenanya. Meski sampai saat ini tidak jago ngoding (cuma modal copas) tetapi aku pikir kuliah di PNC memberiku landasan yang baik`,
+  "sofa-table": `Hai, Selamat Datang di Rumahku, Perkenalkan aku Oni (23), aku tinggal di Ciamis dan sekarang bekerja di Cilacap sebagai editor buku. Hobiku diantaranya : memancing, fotograpi dan membuat program/apliaksi sederhana`,
 
-  tv: `Hai, Selamat Datang di Rumah saya, Perkenalkan saya Oni (23), saya tinggal di Ciamis dan sekarang bekerja di Cilacap sebagai editor buku. Hobi saya diantaranya : memancing, fotograpi dan membuat program/apliaksi sederhana`,
-  bed: `Di sinilah saya tidur. Mimpi dan ide-ide hebat datang ketika saya sedang berbaring di tempat tidur. Diantara adalah saya bermimpi ingin membuat game-game pixel seperti Stardey Valley`,
+  tv: `Ini TV di rumah ku. Aku suka bersantai di sofa sambil menonton YouTube / TV. Di Youtube aku suka nonton channel DEANKT & WPU. Di TV aku cuma nonton bola, btw aku fans MU (3x Juara UCL)`,
+  bed: `Di sinilah aku tidur. Mimpi dan ide-ide hebat datang ketika aku sedang berbaring di tempat tidur. Diantara adalah aku bermimpi ingin membuat game-game pixel seperti Stardey Valley`,
   resume: `404`,
-  projects: `Beberapa project yang pernah saya kerjakan, diantaranya : <br> 1. Aplikasi Kasir Kafe Berbasis Desktop dengan Bahasa C#<br> 2. Sistem Informasi Pembelian Tiket Kereta Berbasis Website <br> 3. Sistem Informasi Tabungan Siswa Berbasis Website <br> 4. Sistem Informasi Pembayaran SPP Berbasis Website <br> 5. Sistem Informasi Praktik Kerja Lapangan Berbasis Website <br> 6. Sistem Informasi Edit Buku Berbasis Website`,
-  library: `Ada beberapa Skill yang saya kuasai diantaranya : Membuat aplikasi berbasis web & desktop, Membuat design/mockup web di Figma & Balsamiq, menggunakan Microsoft Office (Word, Excel, PowerPoint), Melakukan editing di aplikasi Lightroom & Canva?`,
-  exit: `Jika Anda ingin keluar dari web portofolio ini, tutup saja tabnya.`,
+  projects: `Beberapa project yang pernah aku kerjakan, diantaranya : <br> 1. Aplikasi Kasir Kafe Berbasis Desktop dengan Bahasa C#<br> 2. Sistem Informasi Pembelian Tiket Kereta Berbasis Website <br> 3. Sistem Informasi Tabungan Siswa Berbasis Website <br> 4. Sistem Informasi Pembayaran SPP Berbasis Website <br> 5. Sistem Informasi Praktik Kerja Lapangan Berbasis Website <br> 6. Sistem Informasi Edit Buku Berbasis Website`,
+  library: `Ada beberapa Skill yang aku kuasai diantaranya : Membuat aplikasi berbasis web & desktop, Membuat design/mockup web di Figma & Balsamiq, menggunakan Microsoft Office (Word, Excel, PowerPoint), Melakukan editing di aplikasi Lightroom & Canva?`,
+  exit: `Jika Kamu ingin keluar dari web portofolio ini, tutup saja tabnya.`,
 };
+
+
