ebc9226fa97c25d852102b51d99dbe703428a0c1
onips <oni.user01@gmail.com>
2024-11-29 15:35:12 +0700

    Update skill

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const dialogueData = {
   "sofa-table": `Hai, Selamat Datang di Rumahku, Perkenalkan aku Oni (23), aku tinggal di Ciamis dan sekarang bekerja di Cilacap sebagai editor buku. Hobiku diantaranya : memancing, fotografi dan membuat program/apliaksi sederhana`,
 
   tv: `Ini TV di rumah ku. Aku suka bersantai di sofa sambil menonton YouTube / TV. Di Youtube aku suka nonton channel DEANKT & WPU. Di TV aku cuma nonton bola, btw aku fans MU (3x Juara UCL)`,
-  bed: `Di sinilah aku tidur. Mimpi dan ide-ide hebat datang ketika aku sedang berbaring di tempat tidur`,
+  bed: `Di sinilah aku tidur. Mimpi dan ide-ide hebat datang ketika aku sedang berbaring di tempat ini`,
   resume: `404`,
   projects: `Beberapa project yang pernah aku kerjakan, diantaranya : 
               <br> 1. Sistem Informasi Edit Buku Berbasis Website - PHP (2024)
